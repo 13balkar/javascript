732116285ac0bdcdf17a99ac356d28535c2772cf
Balkar_Singh <balkar81199@gmial.com>
2023-01-13 16:16:51 +0530

    feat: add numbers checker

--- numberChecker.js
+++ numberChecker.js
@@ -15,7 +15,7 @@ const checkPrime=(input) =>{
 }
 function isArmstrong(input){
     let stringForm=input.toString();
-    let numberSize=stringForm.length();
+    let numberSize=stringForm.length;
     let out=0;
     const temp=input;
     while(input>0){
@@ -31,14 +31,18 @@ const checkArmstrong=(input)=>{
     let numberSize=stringForm.length;
     let out=0;
     const temp=input;
-    while(input>0){
+    let itr=0;
+
+    while(itr<numberSize){
         let reminder=input%10;
         out+=Math.pow(reminder,numberSize);
+        itr++;
         input/=10;
     }
+    console.log(out);
     return out===temp ? true:false;
 }
-console.log(isPrime(13));
-console.log(isArmstrong(153));
+console.log(checkPrime(13));
+console.log(checkArmstrong(153));
 
 // do it by arrow function
