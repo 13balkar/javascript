e2be5e22a8e3895f8b6d012fc7276ee0dd2ff32b
Balkar_Singh <balkar81199@gmial.com>
2023-01-14 12:29:48 +0530

    feat: use arrow function and immediately invoked functions

--- differentFunctionTypes.js
+++ differentFunctionTypes.js
@@ -25,3 +25,45 @@ const userDetails = {
 // userDetails.thisInArrow()
 
 console.log(this);
+
+
+
+var stores = "store-1";
+// console.log(stores);
+
+// 1. scope - var: global, let: scope
+// 2. initial - var: undefined, let: uninit
+// 3. var and let - change, const - final
+// 4. arrays - reference to the value - const
+
+let index = 1;
+
+const nextIndex = 1;
+// nextIndex = nextIndex + 1;
+const array = [1,2,3]
+array.push(4);
+
+// array = [5,6,7]
+console.log(array);
+let def;
+
+{
+    console.log(stores);
+    console.log(array);
+    console.log(index);
+    var abc = 1;
+    const def = 1;
+    // let ghi = 1;
+}
+
+console.log(abc);
+console.log(def);
+
+// are null and undefined same?
+
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(2 == '2');
+console.log(2 === '2');
+console.log(2 === 2);
+
